be4c600619872200bcbce50c1c8d13f4699c7117
yu-ri <yu113mu@gmail.com>
2026-07-21 10:09:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ $(function () {
 
 
 
-   /*=================================================
-  top-mainvisual スライダー
+  /*=================================================
+  top-mainvisual フェード
   ===================================================*/
-  
+
   $(".bg-slider").slick({
     arrows: false,
     slidesToShow: 1,
@@ -29,8 +29,8 @@ $(function () {
     arrows: false, // 矢印
     dots: false,
     fade: true,
-  speed: 1000,
-  cssEase: "ease-in-out",
+    speed: 1000,
+    cssEase: "ease-in-out",
     // インジケーター
     responsive: [
       {
@@ -41,10 +41,47 @@ $(function () {
       },
     ],
   });
+
  /*=================================================
-  worksshingle 選択した画像を緑に
+   下層ページcompany フェード
   ===================================================*/
-  $('.thumbnail').click(function(){
+  $(".company-photo").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    speed: 1000,
+    cssEase: "ease-in-out",
+
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+  });
+
+  /*=================================================
+   下層ページrecruit フェード
+  ===================================================*/
+  $(".recruit-image").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    speed: 1000,
+    cssEase: "ease-in-out",
+
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    arrows: false,
+    dots: false,
+  });
+
+
+
+  /*=================================================
+   worksshingle 選択した画像を緑に
+   ===================================================*/
+  $('.thumbnail').click(function () {
 
     //クリックした画像のsrcを取得
     const image = $(this).find('img').attr('src');
@@ -58,17 +95,73 @@ $(function () {
     //クリックしたものだけactive
     $(this).addClass('active');
 
-});
-
-   /*=================================================
-  アコーディオン
+  })
+  
+  /*=================================================
+  worksshingle スマホサムネイル
   ===================================================*/
 
-    //.accordion_oneの中の.accordion_headerがクリックされたら
-    $('.recruit-faq-list .faq-button').click(function () {
-      $(this).closest('dt').next('.faq-answer').slideToggle();
+  $(document).on("click", ".works-thumb-prev", function () {
+    const grid = $(this).siblings(".works-grid").get(0);
+  
+    grid.scrollBy({
+      left: -grid.clientWidth,
+      behavior: "smooth",
+    });
   });
+  
+  $(document).on("click", ".works-thumb-next", function () {
+    const grid = $(this).siblings(".works-grid").get(0);
+  
+    grid.scrollBy({
+      left: grid.clientWidth,
+      behavior: "smooth",
+    });
   });
+
+  /*=================================================
+   下層ページabout フェード
+   ===================================================*/
+
+  const $nextEraSlider = $(".next-era-photo");
+
+  function toggleNextEraSlider() {
+    const isMobile = window.innerWidth <= 768;
+
+    if (isMobile && !$nextEraSlider.hasClass("slick-initialized")) {
+      $nextEraSlider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        autoplay: false,
+        swipe: true,
+        touchMove: true,
+        fade: true,
+        speed: 500,
+        cssEase: "ease-in-out",
+      });
+    }
+
+    if (!isMobile && $nextEraSlider.hasClass("slick-initialized")) {
+      $nextEraSlider.slick("unslick");
+    }
+  }
+
+  toggleNextEraSlider();
+  $(window).on("resize", toggleNextEraSlider);
+
+
+  /*=================================================
+ アコーディオン
+ ===================================================*/
+
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $('.recruit-faq-list .faq-button').click(function () {
+    $(this).closest('dt').next('.faq-answer').slideToggle();
+  });
+});
 
 
 
